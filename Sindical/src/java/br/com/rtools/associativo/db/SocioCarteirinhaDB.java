ec7a03ccab39df17072e32261fb88822a86fb2a5
suporte.rtools@gmail.com <suporte.rtools@gmail.com@a619abfb-50b4-7158-5910-c9b4b6ee83e0>
2013-10-08 20:01:06 +0000

    SOCIO CARTEIRINHA DB
    Código formatado;
    
    Bruno

--- Sindical/src/java/br/com/rtools/associativo/db/SocioCarteirinhaDB.java
+++ Sindical/src/java/br/com/rtools/associativo/db/SocioCarteirinhaDB.java
@@ -4,14 +4,24 @@ import br.com.rtools.associativo.SocioCarteirinha;
 import java.util.List;
 
 public interface SocioCarteirinhaDB {
+
     public SocioCarteirinha pesquisaCodigo(int id);
+
     public List pesquisaTodos();
+
     public List pesquisaSocioSemCarteirinha();
+
     public List pesquisaSocioSemCarteirinhaDependente();
+
     public List pesquisaSocioCarteirinhaSocio(int idSocio);
-    public List listaFiltro (String indexFiltro, String descEmpresa, String indexOrdem, boolean fantasia);
+
+    public List listaFiltro(String indexFiltro, String descEmpresa, String indexOrdem, boolean fantasia);
+
     public List filtroCartao(int id_socio);
+
     public List listaFiltroCartao(int id_socio);
+
     public List listaPesquisaEtiqueta(int id_pessoa);
+
     public boolean verificaSocioCarteirinhaExiste(int id_socio);
 }
