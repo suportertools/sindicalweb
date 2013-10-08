103eae67a8e7f974974e6733af49f0c44aeaf596
Rtools <suportertools@users.noreply.github.com>
2013-10-08 19:49:46 +0000

    ATALHO
    Código formatado
    
    Bruno

--- Sindical/src/java/br/com/rtools/sistema/db/AtalhoDB.java
+++ Sindical/src/java/br/com/rtools/sistema/db/AtalhoDB.java
@@ -5,9 +5,14 @@ import br.com.rtools.sistema.ContadorAcessos;
 import java.util.List;
 
 public interface AtalhoDB {
+
     public List<Atalhos> listaTodos(int id_pessoa);
+
     public Atalhos pesquisaPorSigla(String sigla);
+
     public Atalhos pesquisaPorRotina(int id_rotina);
+
     public List<ContadorAcessos> listaAcessosUsuario(int id_usuario);
+
     public ContadorAcessos pesquisaContadorAcessos(int id_usuario, int id_rotina);
 }
