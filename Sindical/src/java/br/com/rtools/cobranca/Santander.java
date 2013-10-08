e423c0c106821607a916479221cf3852b894dca4
suporte.rtools@gmail.com <suporte.rtools@gmail.com@a619abfb-50b4-7158-5910-c9b4b6ee83e0>
2013-10-08 20:03:48 +0000

    SANTANDER
    ?????? haviam modificações, feitas por ????? não sei, perguntei a todos.
    
    Bruno

--- Sindical/src/java/br/com/rtools/cobranca/Santander.java
+++ Sindical/src/java/br/com/rtools/cobranca/Santander.java
@@ -1,15 +1,12 @@
-
-
 package br.com.rtools.cobranca;
 
 import br.com.rtools.financeiro.Boleto;
 import br.com.rtools.financeiro.Movimento;
 import br.com.rtools.utilitarios.Moeda;
 
+public class Santander extends Cobranca {
 
-public class Santander extends  Cobranca{
-
-    public Santander(Movimento movimento, Boleto boleto){
+    public Santander(Movimento movimento, Boleto boleto) {
         super(movimento, boleto);
     }
 
@@ -20,29 +17,30 @@ public class Santander extends  Cobranca{
         int soma = 0;
         int swap = 0;
         String m;
-        while (i > 0){
+        while (i > 0) {
             m = composicao.substring(i - 1, i);
             swap = Integer.parseInt(m) * j;
-            if (swap > 9){
+            if (swap > 9) {
                 String num = Integer.toString(swap);
                 swap = Integer.parseInt(num.substring(0, 1)) + Integer.parseInt(num.substring(1, 2));
             }
             soma += swap;
-            if (j == 2){
+            if (j == 2) {
                 j = 1;
-            }else if (j == 1){
+            } else if (j == 1) {
                 j = 2;
             }
             i--;
         }
 
-        if (((10 - (soma % 10)) == 10) || ((soma % 10) == 0)){
+        if (((10 - (soma % 10)) == 10) || ((soma % 10) == 0)) {
             return "0";
-        }else{
-            if (soma < 10)
+        } else {
+            if (soma < 10) {
                 return Integer.toString(10 - soma);
-            else
+            } else {
                 return Integer.toString(10 - (soma % 10));
+            }
         }
     }
 
@@ -52,8 +50,8 @@ public class Santander extends  Cobranca{
         int j = 2;
         int soma = 0;
         String m;
-        while (i > 0){
-            if (j > 9){
+        while (i > 0) {
+            if (j > 9) {
                 j = 2;
             }
             m = composicao.substring(i - 1, i);
@@ -61,15 +59,14 @@ public class Santander extends  Cobranca{
             j++;
             i--;
         }
-        if ( ((11 - (soma % 11)) == 0) || ((11 - (soma % 11)) == 1)){
+        if (((11 - (soma % 11)) == 0) || ((11 - (soma % 11)) == 1)) {
             return "0";
-        }else if ((11 - (soma % 11)) > 9){
+        } else if ((11 - (soma % 11)) > 9) {
             return "1";
-        }else{
+        } else {
             return Integer.toString(11 - (soma % 11));
         }
     }
-
 
     @Override
     public String codigoBarras() {
@@ -78,7 +75,7 @@ public class Santander extends  Cobranca{
         codigoBarras += fatorVencimento(movimento.getDtVencimento());   // fator de vencimento
         int i = 0;
         int tam = Moeda.limparPonto(Moeda.converteR$Float(movimento.getValor())).length();
-        while ( i != ( 10 -  tam )){ // zeros
+        while (i != (10 - tam)) { // zeros
             codigoBarras += "0";
             i++;
         }
@@ -108,19 +105,19 @@ public class Santander extends  Cobranca{
         repNumerica += codigoBarras.substring(4, 5);
         swap += codigoBarras.substring(5, 19);
         i = 0;
-        while (i < (15 - swap.length())){
+        while (i < (15 - swap.length())) {
             swap = ("0" + swap);
             i++;
         }
         repNumerica += swap;
-        repNumerica = repNumerica.substring(0, 5) + "." +
-                      repNumerica.substring(5, 10) + " " +
-                      repNumerica.substring(10, 15) + "." +
-                      repNumerica.substring(15, 21) + " " +
-                      repNumerica.substring(21, 26) + "." +
-                      repNumerica.substring(26, 32) + " " +
-                      repNumerica.substring(32, 33) + " " +
-                      repNumerica.substring(34, repNumerica.length());
+        repNumerica = repNumerica.substring(0, 5) + "."
+                + repNumerica.substring(5, 10) + " "
+                + repNumerica.substring(10, 15) + "."
+                + repNumerica.substring(15, 21) + " "
+                + repNumerica.substring(21, 26) + "."
+                + repNumerica.substring(26, 32) + " "
+                + repNumerica.substring(32, 33) + " "
+                + repNumerica.substring(34, repNumerica.length());
         return repNumerica;
     }
 
@@ -143,8 +140,4 @@ public class Santander extends  Cobranca{
     public String codigoBanco() {
         return "033-7";
     }
-
 }
-
-
-
