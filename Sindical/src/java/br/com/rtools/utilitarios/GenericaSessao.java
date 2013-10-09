4a5a3ca75d8bdabdd546145147d2b132d0965cfd
Rtools <suportertools@users.noreply.github.com>
2013-10-09 19:26:11 +0000

    Formatação de código (Feita pela ferramenta do Netbeans)
    Bruno

---
+++ Sindical/src/java/br/com/rtools/utilitarios/GenericaSessao.java
@@ -0,0 +1,44 @@
+package br.com.rtools.utilitarios;
+
+import javax.faces.context.FacesContext;
+
+public class GenericaSessao {
+
+    public static void put(String sessionName, String sessionValue) {
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(sessionName, sessionValue);
+    }
+
+    public static void put(String sessionName, Object sessionValue) {
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(sessionName, sessionValue);
+    }
+    
+    public static String getString(String sessionName) {
+        String string = "";
+        if (FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(sessionName) != null) {
+            string = (String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(sessionName);
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove(sessionName);
+        }
+        return string;
+    }
+
+    public static Object getObject(String sessionName) {
+        Object object = new Object();
+        if (FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(sessionName) != null) {
+            object = (Object) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(sessionName);
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove(sessionName);
+        }
+        return object;
+    }
+    
+    public static void remove(String sessionName) {
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove(sessionName);
+    }
+    
+    public static boolean exists(String sessionName) {
+        if (FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(sessionName) != null) {
+            return true;
+        } else {
+            return false;            
+        }
+    }    
+}
