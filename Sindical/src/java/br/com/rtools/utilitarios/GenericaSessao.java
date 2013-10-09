eba93b74699924eb4bb48dec731ebc4a1c8126e3
suporte.rtools@gmail.com <suporte.rtools@gmail.com@a619abfb-50b4-7158-5910-c9b4b6ee83e0>
2013-10-09 20:10:52 +0000

    Formatação de código (Feita pela ferramenta do Netbeans)
    Bruno

--- Sindical/src/java/br/com/rtools/utilitarios/GenericaSessao.java
+++ Sindical/src/java/br/com/rtools/utilitarios/GenericaSessao.java
@@ -11,20 +11,20 @@ public class GenericaSessao {
     public static void put(String sessionName, Object sessionValue) {
         FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(sessionName, sessionValue);
     }
-    
+
     public static String getString(String sessionName) {
         String string = "";
         if (FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(sessionName) != null) {
             string = (String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(sessionName);
-            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove(sessionName);                
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove(sessionName);
         }
         return string;
     }
-    
+
     public static String getString(String sessionName, boolean remove) {
         String string = GenericaSessao.getString(sessionName);
         if (remove) {
-            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove(sessionName);                
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove(sessionName);
         }
         return string;
     }
@@ -36,24 +36,24 @@ public class GenericaSessao {
         }
         return object;
     }
-    
+
     public static Object getObject(String sessionName, boolean remove) {
         Object object = GenericaSessao.getObject(sessionName);
         if (remove) {
-            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove(sessionName);                
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove(sessionName);
         }
         return object;
-    }    
-    
+    }
+
     public static void remove(String sessionName) {
         FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove(sessionName);
     }
-    
+
     public static boolean exists(String sessionName) {
         if (FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(sessionName) != null) {
             return true;
         } else {
-            return false;            
+            return false;
         }
-    }    
+    }
 }
