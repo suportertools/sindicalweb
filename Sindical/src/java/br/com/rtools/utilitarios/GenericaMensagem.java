a9486b09b3e87050f5102a3b7b320a8155f61ba3
Rtools <suportertools@users.noreply.github.com>
2013-10-09 21:04:22 +0000

    Formatação de código (Feita pela ferramenta do Netbeans)
    Bruno

---
+++ Sindical/src/java/br/com/rtools/utilitarios/GenericaMensagem.java
@@ -0,0 +1,23 @@
+package br.com.rtools.utilitarios;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class GenericaMensagem {
+
+    public static void error(String title, String description) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, title, description));        
+    }
+
+    public static void fatal(String title, String description) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, title, description));
+    }
+
+    public static void info(String title, String description) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, title, description));
+    }
+
+    public static void warn(String title, String description) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, title, description));
+    }
+}
