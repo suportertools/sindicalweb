d8245f901f9d39b6a44f5d7d3a7825820a471b4d
suporte.rtools@gmail.com <suporte.rtools@gmail.com@a619abfb-50b4-7158-5910-c9b4b6ee83e0>
2013-10-10 17:13:29 +0000

    Bruno

--- Sindical/src/java/br/com/rtools/financeiro/db/ServicoValorDB.java
+++ Sindical/src/java/br/com/rtools/financeiro/db/ServicoValorDB.java
@@ -1,6 +1,7 @@
 package br.com.rtools.financeiro.db;
 
 import br.com.rtools.financeiro.ServicoValor;
+import java.util.Date;
 import java.util.List;
 
 public interface ServicoValorDB {
@@ -10,4 +11,6 @@ public interface ServicoValorDB {
     public ServicoValor pesquisaServicoValorPorPessoaFaixaEtaria(int idServico, int idPessoa);
 
     public float pesquisaMaiorResponsavel(int idPessoa);
+
+    public float funcaoValorServico(int idPessoa, int idServico, Date date, int tipo);
 }
