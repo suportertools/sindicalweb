22d0c97e38b7bcbdbc6dcb3b3128a34374f28c3e
suporte.rtools@gmail.com <suporte.rtools@gmail.com@a619abfb-50b4-7158-5910-c9b4b6ee83e0>
2014-03-11 11:13:27 +0000

    Acrescentado uma nova rotina: 'RESCISÃO DE CONTRATO', '"/Sindical/rescisaoMatriculaEscola.jsf"';
    Acrescentado um novo tipo de serviço financeiro: Multa;
    
    Programador: Bruno

--- Sindical/web/SQL/INSERIR DADOS.sql
+++ Sindical/web/SQL/INSERIR DADOS.sql
@@ -393,6 +393,7 @@ SELECT setval('seg_nivel_id_seq', max(id)) FROM seg_nivel;
 -- INSERT INTO seg_rotina (id, ds_rotina, ds_nome_pagina, ds_classe, is_ativo) SELECT 224, 'DEPOSITO BANCÁRIO', '"/Sindical/depositoBancario.jsf"', '', true WHERE NOT EXISTS ( SELECT id FROM seg_rotina WHERE id = 224);
 -- INSERT INTO seg_rotina (id, ds_rotina, ds_nome_pagina, ds_classe, is_ativo) SELECT 225, 'MOVIMENTO BANCÁRIO', '"/Sindical/movimentoBancario.jsf"', '', true WHERE NOT EXISTS ( SELECT id FROM seg_rotina WHERE id = 225);
 -- INSERT INTO seg_rotina (id, ds_rotina, ds_nome_pagina, ds_classe, is_ativo) SELECT 226, 'RELATÓRIO CONVITE CLUBE', '"/Sindical/relatorioConviteClube.jsf"', '', true WHERE NOT EXISTS ( SELECT id FROM seg_rotina WHERE id = 226);
+-- INSERT INTO seg_rotina (id, ds_rotina, ds_nome_pagina, ds_classe, is_ativo) SELECT 227, 'RESCISÃO DE CONTRATO', '"/Sindical/rescisaoMatriculaEscola.jsf"', '', true WHERE NOT EXISTS ( SELECT id FROM seg_rotina WHERE id = 227);
 -- SELECT setval('seg_rotina_id_seq', max(id)) FROM seg_rotina;
 
 -- arr_motivo_inativacao
@@ -722,6 +723,7 @@ INSERT INTO fin_tipo_servico (id, ds_descricao) SELECT 2, 'Admissional' WHERE NO
 INSERT INTO fin_tipo_servico (id, ds_descricao) SELECT 3, 'Complemento' WHERE NOT EXISTS ( SELECT id FROM fin_tipo_servico WHERE id = 3);
 INSERT INTO fin_tipo_servico (id, ds_descricao) SELECT 4, 'Acordo' WHERE NOT EXISTS ( SELECT id FROM fin_tipo_servico WHERE id = 4);
 INSERT INTO fin_tipo_servico (id, ds_descricao) SELECT 5, 'Taxa' WHERE NOT EXISTS ( SELECT id FROM fin_tipo_servico WHERE id = 5);
+INSERT INTO fin_tipo_servico (id, ds_descricao) SELECT 6, 'Multa' WHERE NOT EXISTS ( SELECT id FROM fin_tipo_servico WHERE id = 6);
 SELECT setval('fin_tipo_servico_id_seq', max(id)) FROM fin_tipo_servico;
 
 -- pes_tipo_centro_comercial 
